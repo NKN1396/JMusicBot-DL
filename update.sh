caafbb800e36a8ce9e1b5bfd377be5aa0fd1087a
NKN1396 <someone.took.nkn1396@gmail.com>
2021-01-14 03:09:41 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# This script downloads the latest version of JMusicBot.
+# Version 2 - (C) 2021 - NKN1396
+
+# Figure out newest file
+URL=$(
+  curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest \
+  | jq -r '.assets[0].browser_download_url'
+)
+
+# Download file
+wget -nv $URL
+
+# Set permissions
+echo $URL \
+| grep -o -P "[^\\/]+.jar$" \
+| xargs chmod u+x
